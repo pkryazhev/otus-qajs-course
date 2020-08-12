79851e43cb4119b551ff5ffedea92a76cc043424
Pavel Kryazhev <kryazhevpavel94@gmail.com>
2020-08-12 19:10:22 +0300

    add assert in test

--- tests/puppeteer_task_test.js
+++ tests/puppeteer_task_test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const assert = require('assert');
+
 Feature('puppeteer add task test');
 
 Before(async function () { // or Background
@@ -14,24 +16,32 @@ After(async function () { // or Background
     await browser.close();
 });
 
+/*
 Scenario('add task', async function () {
     await page.waitForSelector('#new-todo');
     await page.type('#new-todo', 'test task');
     await page.keyboard.press('Enter');
     await page.waitForXPath('//label[contains(text(), "test task")]');
 });
+*/
 
 Scenario('add second task', async function () {
     await page.waitForSelector('#new-todo');
     await page.type('#new-todo', 'test task');
     await page.keyboard.press('Enter');
     await page.waitForXPath('//label[contains(text(), "test task")]');
+    const element = await page.$("#todo-count > strong");
+    let text = await page.evaluate(element => element.textContent, element);
+    assert.equal(text, 1);
     await page.waitForSelector('#new-todo');
     await page.type('#new-todo', 'test second task');
     await page.keyboard.press('Enter');
     await page.waitForXPath('//label[contains(text(), "test second task")]');
+    text = await page.evaluate(element => element.textContent, element);
+    assert.equal(text, 2);
 });
 
+/*
 Scenario('remove task', async function () {
     await page.waitForSelector('#new-todo');
     await page.type('#new-todo', 'task for remove');
@@ -61,3 +71,4 @@ Scenario('edit task', async function () {
     await page.keyboard.press('Enter');
     await page.waitForXPath('//label[contains(text(), "task for edit + new text")]');
 });
+*/
